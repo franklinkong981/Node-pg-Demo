77e5bf3b903648f3124c574374b4d7dffd0d6f08
Franklin Kong <franklinkong981@gmail.com>
2024-10-14 21:46:22 -0700

    Wrote routes for user

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,52 @@ const router = express.Router();
 const db = require("../db");
 
 router.get('/', async (req, res) => {
-  const results = await db.query(`SELECT * FROM users`);
-  return HTMLTableRowElement.json(results.rows);
+  try {
+    const results = await db.query(`SELECT * FROM users`);
+    return res.status(200).json(results.rows);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.get('/search', async (req, res, next) => {
+  try {
+    const {type} = req.query;
+    const results = await db.query(`SELECT * FROM uers WHERE type=$1`, [type]);
+    return res.status(200).json(results.rows);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {name, type} = req.body;
+    const results = await db.query(`INSERT INTO users (name, type) VALUES ($1, $2) RETURNING *`, [name, type]);
+    return res.status(201).json(results.rows[0]);
+  } catch(e){
+    return next(e);
+  }
+});
+
+router.patch('/:id', async(req, res, next) => {
+  try {
+    const {id} = req.params;
+    const {name, type} = req.body;
+    const results = await db.query('UPDATE users SET name=$1, type=$2 WHERE id=$3 RETURNING *', [name, type, id]);
+    return res.status(204).json(results.rows[0]);
+  } catch(e) {
+    return next(e);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const results = await db.query('DELETE FROM users WHERE id = $1', [req.params.id]);
+    return res.status(204).json({ msg: "DELETED!" });
+  } catch(e) {
+    return next(e);
+  }
 });
 
 module.exports = router;
